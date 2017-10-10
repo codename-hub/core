e8fb7fb6705d2b24279a1ef2a61f71a3fd5e0488
Kevin Dargel <kevin@jocoon.de>
2017-10-10 17:40:15 +0200

    added catchableException

---
+++ backend/class/catchableException.php
@@ -0,0 +1,23 @@
+<?php
+namespace codename\core;
+
+/**
+ * Why should we even use exceptions, if we can't catch them?
+ * Gotta catch 'em all!
+ */
+class catchableException extends \codename\core\exception {
+
+    /**
+     * Create an errormessage
+     * @param string $code
+     * @param int $levels
+     * @param multitype $info
+     */
+    public function __CONSTRUCT(string $code, int $level, $info = null) {
+    	  app::getHook()->fire($code);
+       	app::getHook()->fire('EXCEPTION');
+        return $this;
+    }
+
+
+}
