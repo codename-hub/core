b877f6f815f6ae6b4a39e4532e1a7233905d34aa
Kevin Dargel <kevin@jocoon.de>
2017-10-12 14:53:13 +0200

    fixed domain validator

--- backend/class/validator/text/domain.php
+++ backend/class/validator/text/domain.php
@@ -4,6 +4,16 @@ namespace codename\core\validator\text;
 class domain extends \codename\core\validator\text implements \codename\core\validator\validatorInterface {
 
     /**
+     * @inheritDoc
+     */
+    public function __CONSTRUCT(bool $nullAllowed = false, int $minlength = 0, int $maxlength = 0, string $allowedchars = '', string $forbiddenchars = '' )
+    {
+      // @see https://stackoverflow.com/questions/32290167/what-is-the-maximum-length-of-a-dns-name
+      $value = parent::__CONSTRUCT($nullAllowed, $minlength, 253, $allowedchars, $forbiddenchars);
+      return $value;
+    }
+
+    /**
      *
      * {@inheritDoc}
      * @see \codename\core\validator_interface::validate($value)
@@ -12,23 +22,23 @@ class domain extends \codename\core\validator\text implements \codename\core\val
         if(count(parent::validate($value)) != 0) {
             return $this->errorstack->getErrors();
         }
-        
+
         if(strlen($value) == 0) {
             return $this->errorstack->getErrors();
         }
-        
+
         $domainarr = explode('.', $value);
 
         if(count($domainarr) < 2) {
             $this->errorstack->addError('VALUE', 'NO_PERIOD_FOUND', $value);
             return $this->errorstack->getErrors();
         }
-        
+
         if(gethostbyname($value) == $value) {
             $this->errorstack->addError('VALUE', 'DOMAIN_NOT_RESOLVED', $value);
             return $this->errorstack->getErrors();
         }
-        
+
         return $this->errorstack->getErrors();
     }
 
