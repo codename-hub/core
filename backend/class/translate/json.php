1cf1f4c05a6d1d216c440a38210f0bc56b1c0b16
Kevin Dargel <kevin@jocoon.de>
2017-10-12 11:45:49 +0200

    allow json translation inheritance added default exception translation driver, accessible via app::getTranslate('exception')->translate('EXCEPTION.<your-exception-code>')

--- backend/class/translate/json.php
+++ backend/class/translate/json.php
@@ -4,18 +4,24 @@ namespace codename\core\translate;
 use \codename\core\app;
 
 class json extends \codename\core\translate implements \codename\core\translate\translateInterface {
-    
+
     /**
      * I did not find the requested translation file.
      * @var string
      */
     CONST EXCEPTION_GETTRANSLATION_TRANSLATIONFILEMISSING = 'EXCEPTION_GETTRANSLATION_TRANSLATIONFILEMISSING';
-    
+
     /**
      * The translation file that I managed to find was invalid.
      * @var string
      */
     CONST EXCEPTION_GETTRANSLATION_TRANSLATIONFILEINVALID = 'EXCEPTION_GETTRANSLATION_TRANSLATIONFILEINVALID';
+
+    /**
+     * json config reader instances
+     * @var \codename\core\config\json[]
+     */
+    protected $instances = array();
 
     /**
      * @todo DOCUMENTATION
@@ -29,33 +35,39 @@ class json extends \codename\core\translate implements \codename\core\translate\
         if(count($key) != 2) {
             return 'KEYLENGTH NOT MATCHING (' . $keystr . ')';
         }
-    
+
         $key[0] = strtolower($key[0]);
         $key[1] = strtoupper($key[1]);
-            
-        $file = app::getInheritedPath('translation/' . app::getInstance('request')->getData('lang') . '/' . $key[0] . '.json');
-    
-        if(!app::getInstance('filesystem_local')->fileAvailable($file)) {
-            throw new \codename\core\exception(self::EXCEPTION_GETTRANSLATION_TRANSLATIONFILEMISSING, \codename\core\exception::$ERRORLEVEL_ERROR, $file);
+
+        $stackname = 'translation/' . app::getInstance('request')->getData('lang') . '/' . $key[0] . '.json';
+
+        /**
+         * [$instance description]
+         * @var \codename\core\config\json
+         */
+        $instance = null;
+
+        if(array_key_exists($stackname, $this->instances)) {
+          $instance = $this->instances[$stackname];
+        } else {
+          $instance = new \codename\core\config\json(
+            $stackname,
+            $this->config['inherit'] ?? false,
+            $this->config['inherit'] ?? false
+          );
         }
-    
-        $file = file_get_contents($file);
-    
-        $file = json_decode($file);
-    
-        if(is_null($file)) {
-            throw new \codename\core\exception(self::EXCEPTION_GETTRANSLATION_TRANSLATIONFILEINVALID, \codename\core\exception::$ERRORLEVEL_FATAL, $file);
-        }
-    
+
         $keyname = $key[1];
         $value = $keyname;
-        if(array_key_exists($key[1], $file)) {
-            $value = $file->$keyname;
+
+        $v = $instance->get($key[1]);
+        if($v != null) {
+          $value = $v;
         } else {
-            \codename\core\app::getHook()->fire(\codename\core\hook::EVENT_TRANSLATE_TRANSLATION_KEY_MISSING, $key);
+          \codename\core\app::getHook()->fire(\codename\core\hook::EVENT_TRANSLATE_TRANSLATION_KEY_MISSING, $key);
         }
-    
+
         return $value;
     }
-    
+
 }
