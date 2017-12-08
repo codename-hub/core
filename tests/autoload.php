e87f6f113b8051fbe911c9246cdcb900ba679442
Kevin Dargel <kevin@jocoon.de>
2017-12-08 15:17:11 +0100

    updated tests/autoload.php

--- tests/autoload.php
+++ tests/autoload.php
@@ -17,16 +17,18 @@
  */
 
 // cross-project autoloader
-$globalBootstrap = realpath(__DIR__.'/../../../../bootstrap.php');
+$globalBootstrap = realpath(__DIR__.'/../../../../bootstrap-cli.php');
 if(file_exists($globalBootstrap)) {
+  echo("Including autoloader at " . $globalBootstrap . chr(10) );
   require_once $globalBootstrap;
 } else {
-  die("ERROR: No global bootstrap.php found. You might want to initialize your cross-project autoloader using the root composer.json first." . chr(10) );
+  die("ERROR: No global bootstrap.cli.php found. You might want to initialize your cross-project autoloader using the root composer.json first." . chr(10) );
 }
 
 // local autoloader
 $localAutoload = realpath(__DIR__.'/../vendor/autoload.php');
 if(file_exists($localAutoload)) {
+  echo("Including autoloader at " . $localAutoload . chr(10) );
   require_once $localAutoload;
 } else {
   die("ERROR: No local vendor/autoloader.php found. Please call \"composer dump-autoload\" in this directory." . chr(10) );
