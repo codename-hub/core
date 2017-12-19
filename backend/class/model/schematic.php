ae38cddae85c27da6fb2289f254381a19af15c27
Kevin Dargel <kevin@jocoon.de>
2017-12-19 10:03:42 +0100

    fixed model schematic (reset() is now public)

--- backend/class/model/schematic.php
+++ backend/class/model/schematic.php
@@ -7,33 +7,33 @@ namespace codename\core\model;
  * @since 2016-02-04
  */
 abstract class schematic extends \codename\core\model {
-    
+
     /**
      * Contains the schema this model is based upon
      * @var string
      */
     public $schema = null;
-    
+
     /**
      * Contains the table this model is based upon
      * @var string
      */
     public $table = null;
-    
+
     /**
      * Contains more models
      * @var unknown $tables
      */
     protected $tables = array();
-    
+
     /**
      * resets all the parameters of the instance for another query
      * @return void
      */
-    protected function reset() {
+    public function reset() {
         parent::reset();
         $this->tables = array();
         return;
     }
-    
+
 }
