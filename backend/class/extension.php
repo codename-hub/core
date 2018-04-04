8f17401e779058777ac50487d4496f027ace74bb
Kevin <kevin@jocoon.de>
2018-04-04 10:55:19 +0200

    added extension base class for future extension implementations

---
+++ backend/class/extension.php
@@ -0,0 +1,35 @@
+<?php
+namespace codename\core;
+
+/**
+ * extension base class
+ */
+abstract class extension extends \codename\core\bootstrap {
+
+  /**
+   * [getExtensionName description]
+   * @return string [description]
+   */
+  public abstract function getExtensionName() : string;
+
+  /**
+   * [getExtensionVendor description]
+   * @return string [description]
+   */
+  public abstract function getExtensionVendor() : string;
+
+  /**
+   * Returns parameters used for injecting the extension
+   * into an appstack
+   *
+   * @return array [injection parameters]
+   */
+   final public function getInjectParameters() : array {
+    return [
+      'vendor'    => $this->getExtensionVendor(),
+      'app'       => $this->getExtensionName(),
+      'namespace' => __NAMESPACE__
+    ];
+  }
+
+}
