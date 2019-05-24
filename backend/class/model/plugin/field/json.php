d08dcaabe7e7e70d4b8654f089415352189a9ddb
Kevin <kevin@jocoon.de>
2019-05-24 15:12:22 +0200

    added model plugin\field\json, just for completeness and support

---
+++ backend/class/model/plugin/field/json.php
@@ -0,0 +1,12 @@
+<?php
+namespace codename\core\model\plugin\field;
+
+/**
+ * Bare field plugin
+ * @package core
+ * @author Kevin Dargel
+ * @since 2019-04-18
+ */
+class json extends \codename\core\model\plugin\field implements \codename\core\model\plugin\field\fieldInterface {
+
+}
