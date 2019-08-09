d9c4dd1495249e9e56f2f75b0f4469e11391c4d4
Ralf <weltherrschaft@granade.eu>
2019-08-09 14:20:40 +0200

    update value storage

--- backend/class/value/structure.php
+++ backend/class/value/structure.php
@@ -2,5 +2,9 @@
 namespace codename\core\value;
 
 class structure extends \codename\core\value {
-    
+  /**
+   * This validator is used to validate the value on generation.
+   * @var string
+   */
+  protected $validator = 'structure';
 }
