005551eda11fc9868746cd82db22a134d097a007
Kevin <kevin@jocoon.de>
2019-10-07 16:12:04 +0200

    value text_actionname now relies on validator methodname

--- backend/class/value/text/actionname.php
+++ backend/class/value/text/actionname.php
@@ -7,6 +7,6 @@ class actionname extends \codename\core\value\text {
      * {@inheritDoc}
      * @see \codename\core\value::$validator
      */
-    protected $validator = 'text_apploader';
-    
+    protected $validator = 'text_methodname';
+
 }
