57028a339aa47c5217582d267c360f6781ae5cab
Kevin <kevin@jocoon.de>
2019-11-01 12:04:40 +0100

    added validator structure_text_telephone (prototype, testing)

---
+++ backend/class/validator/structure/text/telephone.php
@@ -0,0 +1,43 @@
+<?php
+namespace codename\core\validator\structure\text;
+
+use codename\core\app;
+
+/**
+ * validator that validates multiple telephone numbers at once
+ */
+class telephone extends \codename\core\validator\structure
+{
+  /**
+   * [protected description]
+   * @var \codename\core\validator
+   */
+  protected $elementValidator = null;
+
+  /**
+   * @inheritDoc
+   */
+  public function __CONSTRUCT(bool $nullAllowed = true)
+  {
+    parent::__CONSTRUCT($nullAllowed);
+    $this->elementValidator = app::getValidator('text_telephone');
+  }
+
+  /**
+   * @inheritDoc
+   */
+  public function validate($value) : array
+  {
+    parent::validate($value);
+
+    if(is_array($value)) {
+      foreach($value as $phoneNumber) {
+        if(count($errors = $this->elementValidator->reset()->validate($phoneNumber)) > 0) {
+          $this->errorstack->addError('VALUE', 'INVALID_PHONE_NUMBER', $errors);
+        }
+      }
+    }
+
+    return $this->getErrors();
+  }
+}
