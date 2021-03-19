7b896d754e3e510029c8225dc10a4ff9eea60316
Kevin <kevin@jocoon.de>
2021-03-19 16:56:19 +0100

    added collection model test in virtualFieldsTest

--- tests/virtualFieldsTest.php
+++ tests/virtualFieldsTest.php
@@ -76,6 +76,7 @@ class virtualFieldsTest extends base {
         'model2_value',
         'model2_model1_id',
         'model2_model1',
+        'model2_model3_items'
       ],
       'primary' => [
         'model2_id'
@@ -84,6 +85,16 @@ class virtualFieldsTest extends base {
         'model2_model1' => [
           'type'  => 'foreign',
           'field' => 'model2_model1_id'
+        ],
+        'model2_model3_items' => [
+          'type'  => 'collection',
+        ]
+      ],
+      'collection' => [
+        'model2_model3_items' => [
+          'schema'  => 'schema3',
+          'model'   => 'model3',
+          'key'     => 'model3_model2_id'
         ]
       ],
       'foreign' => [
@@ -100,6 +111,38 @@ class virtualFieldsTest extends base {
         'model2_value'      => 'text',
         'model2_model1_id'  => 'number_natural',
         'model2_model1'     => 'virtual',
+        'model2_model3_items' => 'virtual',
+      ],
+      'connection' => 'default'
+    ]);
+    //
+    // A secondary model with a reference to model1
+    // including a virtual field that may display/represent a model1-dataset
+    //
+    $this->createModel('schema3', 'model3', [
+      'field' => [
+        'model3_id',
+        'model3_created',
+        'model3_modified',
+        'model3_value',
+        'model3_model2_id'
+      ],
+      'primary' => [
+        'model3_id'
+      ],
+      'foreign' => [
+        'model3_model2_id' => [
+          'schema' => 'schema2',
+          'model'  => 'model2',
+          'key'    => 'model2_id'
+        ]
+      ],
+      'datatype' => [
+        'model3_id'         => 'number_natural',
+        'model3_created'    => 'text_timestamp',
+        'model3_modified'   => 'text_timestamp',
+        'model3_value'      => 'text',
+        'model3_model2_id'  => 'number_natural',
       ],
       'connection' => 'default'
     ]);
@@ -159,6 +202,43 @@ class virtualFieldsTest extends base {
     // NOTE: we have reduced field output here!
     $this->assertCount(1, $res);
     $this->assertEquals([ 'model1_value'  => 'def' ], $res[0]['model2_model1']);
+  }
+
+  /**
+   * Tests saving virtual field data with enabled models
+   * and checks result output, including hidden fields
+   * @return void
+   */
+  public function testVirtualFieldWithCollections() {
+    $model2 = $this->getModel('model2')->setVirtualFieldResult(true)
+      ->addCollectionModel($this->getModel('model3'), 'model2_model3_items')
+      ->addModel($model1 = $this->getModel('model1')->setVirtualFieldResult(true))
+    ;
+
+    $model2->saveWithChildren([
+      'model2_value'    => 'xyz',
+      'model2_model3_items' => [
+        [ 'model3_value' => 'first' ],
+        [ 'model3_value' => 'second' ],
+        [ 'model3_value' => 'third' ],
+      ],
+      'model2_model1'  => [
+        'model1_value'  => 'vwu'
+      ]
+    ]);
+    $id = $model2->lastInsertId();
+
+    $res = $model2
+      ->addFilter($model2->getPrimarykey(), $id)
+      ->search()->getResult();
+
+    // Make sure we have the right count being returned
+    // and the data is the same as in the beginning
+    // NOTE: we have reduced field output here!
+    $this->assertCount(1, $res);
+
+    $this->assertEquals([ 'first', 'second', 'third'], array_column($res[0]['model2_model3_items'], 'model3_value'));
+    // $this->assertEquals([ 'model1_value'  => 'def' ], $res[0]['model2_model1']);
   }
 
 }
