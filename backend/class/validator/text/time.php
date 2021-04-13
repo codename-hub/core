7b253e2b857c7195f6615276743c04eeb2204882
Ralf <ralf@jocoon.de>
2021-04-13 10:55:50 +0200

    fixed worng time validator

--- backend/class/validator/text/time.php
+++ backend/class/validator/text/time.php
@@ -9,7 +9,7 @@ class time extends \codename\core\validator\text implements \codename\core\valid
      * @see \codename\core\validator_text::__construct($nullAllowed, $minlength, $maxlength, $allowedchars, $forbiddenchars)
      */
     public function __CONSTRUCT(bool $nullAllowed = false) {
-        parent::__CONSTRUCT($nullAllowed, 1, 32, '0123456789:');
+        parent::__CONSTRUCT($nullAllowed, 1, 8, '0123456789:');
         return $this;
     }
 
@@ -18,25 +18,28 @@ class time extends \codename\core\validator\text implements \codename\core\valid
      */
     public function validate($value): array
     {
-      $errors = parent::validate($value);
+      if(count(parent::validate($value)) != 0) {
+        return $this->errorstack->getErrors();
+      }
+
       $c = explode(':', $value);
       if(count($c) >= 2 && count($c) <= 3) {
         $hours = $c[0];
         $minutes = $c[1];
         $seconds = $c[2] ?? 0;
-        if($hours < 0 && $hours > 23) {
+        if($hours < 0 || $hours > 23) {
           $this->errorstack->addError('VALUE', 'VALUE_INVALID_TIME_HOURS', $value);
         }
-        if($minutes < 0 && $minutes > 59) {
+        if($minutes < 0 || $minutes > 59) {
           $this->errorstack->addError('VALUE', 'VALUE_INVALID_TIME_MINUTES', $value);
         }
-        if($seconds < 0 && $seconds > 59) {
+        if($seconds < 0 || $seconds > 59) {
           $this->errorstack->addError('VALUE', 'VALUE_INVALID_TIME_SECONDS', $value);
         }
       } else {
         $this->errorstack->addError('VALUE', 'VALUE_INVALID_TIME_STRING', $value);
       }
-      return $this->getErrors();
+      return $this->errorstack->getErrors();
     }
 
 }
