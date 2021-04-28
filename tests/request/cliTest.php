f43f85eb6ff80db1d4c566902afbf44e017d34c6
Kevin <kevin@jocoon.de>
2021-04-28 10:48:32 +0200

    marked tests\request\cliTest as incomplete, as it conflicts with the runtime itself

--- tests/request/cliTest.php
+++ tests/request/cliTest.php
@@ -10,7 +10,7 @@ class cliTest extends \codename\core\tests\requestTest {
    * [testDatacontainer description]
    */
   public function testRequestDatacontainer(): void {
-    $this->addWarning('optimize check');
+    $this->markTestIncomplete('CLI Request may contain phpunit/unittest arguments!');
 
     $request = new \codename\core\request\cli();
     $this->assertEquals(array_merge([ 'lang' => 'de_DE']), $request->getData());
