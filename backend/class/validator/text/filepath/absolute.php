2423d8916518fb47086709f5f045f220b37c7c1e
Kevin <kevin@jocoon.de>
2021-04-29 16:03:52 +0200

    made validator text_filepath_absolute compatible with Windows paths (if OS is windows)

--- backend/class/validator/text/filepath/absolute.php
+++ backend/class/validator/text/filepath/absolute.php
@@ -9,7 +9,11 @@ class absolute extends \codename\core\validator\text {
      * @see \codename\core\validator_text::__construct($nullAllowed, $minlength, $maxlength, $allowedchars, $forbiddenchars)
      */
     public function __CONSTRUCT(bool $nullAllowed = false) {
-        parent::__CONSTRUCT($nullAllowed, 1, 256, 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789öäüßÖÄÜabcdefghijklmnopqrstuvwxyz_-./() ');
+        if(strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
+          parent::__CONSTRUCT($nullAllowed, 1, 256, 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789öäüßÖÄÜabcdefghijklmnopqrstuvwxyz_-./()\: ');
+        } else {
+          parent::__CONSTRUCT($nullAllowed, 1, 256, 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789öäüßÖÄÜabcdefghijklmnopqrstuvwxyz_-./() ');
+        }
         return $this;
     }
 
